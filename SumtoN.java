e207b2e93c00c8a0dd85c068ebefdeff5eefdb9a
Sahil Sharrma <129388506+sahillsharma11@users.noreply.github.com>
2023-04-01 00:49:37 +0530

    Add files via upload

---
+++ SumtoN.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class SumtoN{
+public static void main (String[] args){
+  
+Scanner sc =new Scanner(System.in);
+int n = sc.nextInt();
+
+
+int sum = 0;
+while( n !=0){
+
+int temp = n %10;
+
+sum= sum+temp;
+n= n/10;
+
+
+}
+System.out.println(sum);
+return;
+}
+}
+
+
+
+
